5893c7c2899fe8723e587541fcccba632886c54c
Elliott Jin <elliott.jin@gmail.com>
2024-01-05 07:47:52 -0800

    Use Array.every in isSet

--- script.js
+++ script.js
@@ -215,14 +215,11 @@ function toggleSelected(card) {
 }
 
 function isSet(cards) {
-  for (let property of allProperties) {
+  return allProperties.every(property => {
     let values = cards.map(card => card.getAttribute(property));
     let uniqueValues = new Set(values);
-    if (uniqueValues.size !== 1 && uniqueValues.size !== 3) {
-      return false;
-    }
-  }
-  return true;
+    return uniqueValues.size === 1 || uniqueValues.size === 3;
+  });
 }
 
 function boardHasSet() {
